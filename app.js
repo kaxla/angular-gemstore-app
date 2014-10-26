113917067fc27f5d90997488addc97a0180a5cf3
kaxla <kaylas.email.address@gmail.com>
2014-10-26 12:14:50 -0700

    1.5-1.7

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-(function(){
+(function() {
   var app = angular.module('gemStore', []);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Azurite',
-    price: 2.95,
-    description: '...',
-  };
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 },
+  ];
 })();
-
